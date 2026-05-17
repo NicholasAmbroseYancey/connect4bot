ae8a331980822bfd699353fe337f73abe813a590
Nicholas Ambrose Yancey <nicoyancey@gmail.com>
2026-05-17 18:03:08 -0400

    Brushing up the ui in terminal

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+void printBoard(vector<vector<int>> grid) {
+    cout << "\n  1   2   3   4   5   6   7\n"; // Column headers
+    cout << "+---+---+---+---+---+---+---+\n";
+    for (auto row : grid) {
+        cout << "| ";
+        for (int cell : row) {
+            if (cell == 0) {
+                cout << ". | "; // Empty space
+            } else if (cell == 1) {
+                cout << "X | "; // Player 1
+            } else {
+                cout << "O | "; // Player 2
+            }
+        }
+        cout << "\n+---+---+---+---+---+---+---+\n";
+    }
+    cout << endl;
+}
 
 int checkWin(vector<vector<int>> grid, int lastRow, int lastCol, int player) {
     int rows = 6, cols = 7;
     
     int directions[4][2] = {{0, 1}, {1, 0}, {1, 1}, {1, -1}};
     
-    for (auto& dir : directions) {
+    for (auto dir : directions) {
         int dr = dir[0];
         int dc = dir[1];
         int count = 1; 
@@ -41,6 +61,7 @@ int connectFour(vector<vector<int>> grid, int player1, int player2) {
     int player = player1;
 
     while (true) {
+        printBoard(grid);
         int column;
         int currentRow = -1;
         cout << "Player " << player << ", enter a column (1-7): ";
@@ -68,6 +89,7 @@ int connectFour(vector<vector<int>> grid, int player1, int player2) {
         
         int i = checkWin(grid, currentRow, column, player);
         if (i) {
+            printBoard(grid);
             cout << "Player " << player << " wins!" << endl;
             break;
         }
